d9e8370af54f57cd08dcfc848871aadd8df9f560
Anders Johansson <andersjhem@gmail.com>
2025-01-16 07:42:27 +0100

    Changed h2 dep to test

--- src/main/java/org/lexicon/workshopjpa/WorkshopJpaApplication.java
+++ src/main/java/org/lexicon/workshopjpa/WorkshopJpaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WorkshopJpaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WorkshopJpaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopJpaApplication.class, args);
+    }
 
 }
