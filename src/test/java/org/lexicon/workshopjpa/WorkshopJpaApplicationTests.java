849b144f8489ac9d6e578ec456af426cde772f9c
Anders Johansson <andersjhem@gmail.com>
2025-01-16 07:36:13 +0100

    Initial commit 1

---
+++ src/test/java/org/lexicon/workshopjpa/WorkshopJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lexicon.workshopjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
